5ab277371ca5c4db1424d628a029a495524c86aa
Zyrakia <ole.lammers@pm.me>
2024-09-11 11:41:24 -0700

    fix: resolved all requests with utility

--- src/routes/projects.ts
+++ src/routes/projects.ts
@@ -1,6 +1,7 @@
 import { ProjectsController } from '@/controllers/projects.controller.ts';
+import { Resolve } from '@/utils/resolve.ts';
 import { Handler } from 'express';
 
 export const get: Handler = async (req, res) => {
-	res.send(await ProjectsController.getProjects());
+	Resolve(res).okWith(await ProjectsController.getProjects());
 };
